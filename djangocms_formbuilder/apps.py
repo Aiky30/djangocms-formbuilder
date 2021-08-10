0d1ba7a6f1ef20d6390f7ad9e877405c18204e62
Andrew Aikman <andrew.aikman@eliga.services>
2021-08-10 14:39:38 +0100

    Initial project structure and readme added.

---
+++ djangocms_formbuilder/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class DjangocmsFormbuilderConfig(AppConfig):
+    name = 'djangocms_formbuilder'
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='djangocms-formbuilder',
+    version='0.0.0',
+    packages=[],
+    url='',
+    license='BSDv3',
+    author='Aiky30',
+    author_email='',
+    description='A form builder for django cms 4+'
+)
