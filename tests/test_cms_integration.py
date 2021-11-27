5dcc592a8c6ee842c2ee3222690fa4b994cdc014
Aiky30 <aiky30@yahoo.com>
2021-11-27 18:03:54 +0000

    Use a different template to test

--- tests/test_cms_integration.py
+++ tests/test_cms_integration.py
@@ -5,6 +5,6 @@ from cms.test_utils.testcases import CMSTestCase
 class CMSIntegrationTestCase(CMSTestCase):
 
     def test_can_create_page(self):
-        page = create_page("my page", "nav_playground.html", "en")
+        page = create_page("my page", "page.html", "en")
 
         self.assertTrue(page)
